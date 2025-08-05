a0b4403b54a02ad4b3f7b71584cfaeacf4fd2664
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-05 23:47:58 +0530

    Add files via upload

---
+++ 7.Operators.kt
@@ -0,0 +1,39 @@
+var sum1 = 100 + 50       // 150 (100 + 50)
+var sum2 = sum1 + 250     // 400 (150 + 250)
+var sum3 = sum2 + sum2    // 800 (400 + 400)
+
+fun main() {  
+  var x = 5
+  var y = 3
+  println(x - y)  
+}
+
+fun main() {  
+  var x1 = 5
+  var y1 = 3
+  println(x1 * y1)  
+}
+
+fun main() {  
+  var x2 = 12
+  var y2 = 3
+  println(x2 / y2)  
+}
+
+fun main() {  
+  var x3 = 5
+  var y3 = 2
+  println(x3 % y3)  
+}
+
+fun main() {  
+  var x4 = 5
+  ++x4
+  println(x4)  
+}
+
+fun main() {  
+  var x5 = 5
+  --x5
+  println(x5)  
+}
