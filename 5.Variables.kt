e2f09ea2275ac5d2ade3be591b194310e7c0467f
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-05 23:47:32 +0530

    Add files via upload

---
+++ 5.Variables.kt
@@ -0,0 +1,36 @@
+var a = "Spandan"
+val b = "Spandan"
+
+val myNum1: Byte = 100
+Println(myNum1)
+
+val myNum2: Short = 4000
+Println(myNum2)
+
+val myNum3: Int = 411000
+Println(myNum3)
+
+val myNum4: Long = 15000000000L
+println(myNum4)
+
+val myNum5: Float = 5.75F
+println(myNum5)
+
+val myNum6: Double = 19.99
+println(myNum6)
+
+val myNum7: Float = 35E3F
+val myNum8: Double = 12E4
+println(myNum7)
+println(myNum8)
+
+val isKotlinFun: Boolean = true
+val isFishTasty: Boolean = false
+println(isKotlinFun)   // Outputs true
+println(isFishTasty)   // Outputs false 
+
+val myGrade: Char = 'B'
+println(myGrade)
+
+val myText: String = "Hello World"
+println(myText)
