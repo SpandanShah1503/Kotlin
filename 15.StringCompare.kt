751ddc7423eaba88dc982a8351a707e446387dc5
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:43:14 +0530

    Add files via upload

---
+++ 15.StringCompare.kt
@@ -0,0 +1,10 @@
+// The compareTo(string) function compares two strings and returns 0 if both are equal:
+
+fun main() 
+{
+
+  var txt1 = "Hello World"
+  var txt2 = "Hello World"
+  println(txt1.compareTo(txt2))  // Outputs 0 (they are equal)
+  
+}
