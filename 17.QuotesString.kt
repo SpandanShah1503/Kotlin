40915ee3fe0be7b7878ec87a54846e1a394e2c6d
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:46:07 +0530

    Add files via upload

---
+++ 17.QuotesString.kt
@@ -0,0 +1,14 @@
+/*
+Quotes Inside a String
+To use quotes inside a string, use single quotes ('):
+*/
+
+fun main() 
+{
+
+  var txt1 = "It's alright"
+  var txt2 = "That's great"
+  println(txt1) 
+  println(txt2)
+
+}
