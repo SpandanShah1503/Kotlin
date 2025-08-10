f1ab7d670cb550e48dc1861d892420dd85dcc0d7
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:41:39 +0530

    Add files via upload

---
+++ 9.ComparisonOperator.kt
@@ -0,0 +1,25 @@
+fun main() {  
+  var x = 5
+  var y = 3
+  println(x == y) // returns false because 5 is not equal to 3
+
+  var z = 5
+  var b = 3
+  println(z != b) // returns true because 5 is not equal to 3
+
+  var c = 5
+  var d = 3
+  println(c > d) // returns true because 5 is greater than 3
+
+  var e = 5
+  var f = 3
+  println(e < f) // returns false because 5 is not less than 3
+
+  var g = 5
+  var h = 3
+  println(g >= h) // returns true because five is either greater than, or equal, to 3
+
+  var i = 5
+  var j = 3
+  println(i<= j)// returns false because 5 is neither less than or equal to 3
+}
