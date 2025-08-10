5bdd649863283ee010ec36a79e189366f87b3218
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:43:24 +0530

    Add files via upload

---
+++ 16.FindingString.kt
@@ -0,0 +1,14 @@
+/*
+Finding a String in a String
+The indexOf() function returns the index (the position) of the first occurrence of a specified text in a string (including whitespace):
+*/
+
+fun main() 
+{
+
+  var txt = "Please locate where locate occurs!"
+  println(txt.indexOf("locate"))
+
+}
+
+//Output 7
