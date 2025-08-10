451e376b96069e06ed036709fc66c2e285df4bf0
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:41:20 +0530

    Add files via upload

---
+++ 10.LogicalOperator.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+  //AND
+  var x = 5
+  println(x > 3 && x < 10) // returns true because 5 is greater than 3 AND 5 is less than 10
+  //OR
+  var x = 5
+  println(x > 3 || x < 4) // returns true because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+
+  //NOT
+  var myBool = true
+  println(!myBool) // returns false because the ! (not) operator reverses the result
+
+}
