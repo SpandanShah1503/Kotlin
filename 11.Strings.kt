4716334fe1b37c7f3767d1da2d93bc1b3e463712
Spandan Shah <161344413+SpandanShah1503@users.noreply.github.com>
2025-08-10 22:41:52 +0530

    Add files via upload

---
+++ 11.Strings.kt
@@ -0,0 +1,17 @@
+fun()
+{
+  
+  //var a = 'Strings' This will give an error
+  var b = "Strings"
+  print(b)
+
+  var c:String
+  c = "Police"
+  print(c)
+
+  /* This will give an error , If you want to create a String without assigning the value (and assign the value later), you must specify the type while declaring the variable:
+  var d
+  d = "Police"
+  print(d)
+  */
+}
